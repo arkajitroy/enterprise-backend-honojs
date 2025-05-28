c9f13668e72bb7e0b6f600d1c2d9a341d4e09738
arkajitroy <arkajitroy11.official@gmail.com>
2025-05-28 09:37:45 +0530

    refactor: auth-schema and add RBC config

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const registerSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
+  role: z.enum(["USER", "ADMIN"]).optional(),
 });
 
 export const loginSchema = z.object({
